016a0d8bae3dd17d8ebcc57aa86c67c41b4dfc18
Florian Tönjes <skatepainter@yahoo.de>
2017-05-05 16:35:48 +0200

    Exported configuration info config.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 "use strict";
 
 var express = require("express");
-var Twit = require('twit');
+var Twit = require("twit");
 
-var T = new Twit({
-  consumer_key:         '8UUeuzOs7mpca2Bg00z5w4J1g',
-  consumer_secret:      'QRXjPkSkAMfIcnYxeOKz3mEz90UjDYWlT8WeNUaCCpKl5hl14B',
-  access_token:         '860160567713157120-oIYFBRFLD6SfgSQnHeB42LHiIrgz4xg',
-  access_token_secret:  'fMiaBA7cSTSnHVwBdcf0KntcNt6izyyi7co9zRyHQTgYx',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
+var twitterConfig = require("./config");
+
+var T = new Twit(twitterConfig);
 
 var app = express();
 
